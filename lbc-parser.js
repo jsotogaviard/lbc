7c9d98fb6d6bb04295d5e9529ca1f97417c8f515
Jonathan Soto <jonathan.soto@polestar.eu>
2019-02-05 17:22:20 +0100

    more

--- lbc-parser.js
+++ lbc-parser.js
@@ -6,7 +6,9 @@ const schedule = require('node-schedule');
 const config = require('./config.json');
 const credential = require('./credential.json');
 
-schedule.scheduleJob(config.cron_frequency, function(){
+schedule.scheduleJob(config.cron_frequency, function () {
+//schedule.scheduleJob("*/10 * * * * * ", function () {
+  console.log(new Date())
   request(
     {
       method: 'GET',
@@ -15,99 +17,104 @@ schedule.scheduleJob(config.cron_frequency, function(){
         Accept: 'application/json',
       },
     },
-    function(error, response, body) {
-      if(response.statusCode != 200){
-        throw Error(response.statusCode);
-      } 
-      
-      console.log('received data');
-      receivedData = body.match(/<span itemprop="name" data-qa-id="aditem_title".*?>(.*?)<\/span>/g)
-  
-      if (fs.existsSync(config.current)) {
+    function (error, response, body) {
+      if (response.statusCode != 200) {
+        console.log(response.statusCode)
+        console.log(body)
+        console.log(error)
+      } else {
+
+        const regex = /<span itemprop="name" data-qa-id="aditem_title".*?>(.*?)<\/span>/g;
+        const receivedData = []
+        var record = regex.exec(body)
+        while(record !== null){
+          receivedData.push(record[1])
+          record = regex.exec(body)
+        }
+
+        if (fs.existsSync(config.current)) {
           // Read current file
-          fs.readFile(config.current, "utf8",function read(err, storedDataIn) {
+          fs.readFile(config.current, "utf8", function read(err, storedDataIn) {
             if (err) {
-                throw err;
+              throw err;
             }
             storedData = storedDataIn.split("\n")
             if (!arraysAreEqual(receivedData, storedData)) {
-  
+
               // Rotate current file
               var now = new Date();
               const formattedDate = dateFormat(now, "yyyy-mm-dd-hh-MM-ss");
-         
+
               fs.rename(config.current, './data/' + formattedDate + '.csv', function (err) {
                 if (err) throw err;
                 console.log('renamed complete');
-  
+
                 // Persist new 
                 writeCurrent(receivedData)
-  
+
                 // Send email
                 sendEmail(receivedData)
               });
-  
+
             } else {
               console.log('array are equal, no work');
-            } 
-        }); 
-      } else {
-         // Persist new 
-        writeCurrent(receivedData)
-  
-         // Send email
-         sendEmail(receivedData)
+            }
+          });
+        } else {
+          // Persist new 
+          writeCurrent(receivedData)
+
+          // Send email
+          sendEmail(receivedData)
+        }
+      }
     }
-  }
   );
+
 });
 
-function writeCurrent(receivedData){
-      console.log('write current')
-      var file = fs.createWriteStream(config.current);
-      file.on('error', function(err) { /* error handling */ });
-      receivedData.forEach(function(v) { 
-        file.write(v + '\n'); 
-      });
-      file.end();
+function writeCurrent(receivedData) {
+  console.log('write current')
+  var file = fs.createWriteStream(config.current);
+  file.on('error', function (err) { console.log(err) });
+  receivedData.forEach(function (v) {file.write(v + '\n');});
+  file.end();
 }
 
-function sendEmail(receivedDate) {
+function sendEmail(receivedData) {
   let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
-    secure: true, 
+    secure: true,
     auth: {
-      user: credential.user, 
-      pass: credential.pass 
+      user: credential.user,
+      pass: credential.pass
     }
   });
 
-   // setup email data with unicode symbols
-   let mailOptions = {
+  // setup email data with unicode symbols
+  let mailOptions = {
     from: 'LeChat', // sender address
     to: "jsotogaviard@gmail.com, armance.chassaigne@gmail.com", // list of receivers
     subject: "Nouvelle Annonce LBC", // Subject line
     text: receivedData.join("\n")
   };
 
-  transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-      return console.log(error);
-    }
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) { return console.log(error); }
     console.log('Message sent: ' + info.response);
   });
 
   transporter.close();
 }
 
-function arraysAreEqual(x,y){
+function arraysAreEqual(x, y) {
   var objectsAreSame = true;
-  for(var propertyName in x) {
-     if(x[propertyName] !== y[propertyName]) {
-        objectsAreSame = false;
-        break;
-     }
+  for (var propertyName in x) {
+    if (x[propertyName] !== y[propertyName]) {
+      objectsAreSame = false;
+      break;
+    }
   }
   return objectsAreSame;
 }
